d8b7b512ed62e0a8464d389e5f81fda7f5dc8aa4
lachy3414 <lachy3414@gmail.com>
2020-06-23 15:30:02 +0200

    cambiado el home addres

---
+++ precache-manifest.9519084dcd88e380d4c33d4028645886.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612cb9837788b12560a6353a6f97a6b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db91c236410e90c6d12",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "73b19490623e42096842",
+    "url": "/static/css/main.eb36f6a9.chunk.css"
+  },
+  {
+    "revision": "2db91c236410e90c6d12",
+    "url": "/static/js/2.3f039a4c.chunk.js"
+  },
+  {
+    "revision": "37b80d38c6f99bbdda64fccc95f48f68",
+    "url": "/static/js/2.3f039a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b19490623e42096842",
+    "url": "/static/js/main.3bbf0a52.chunk.js"
+  },
+  {
+    "revision": "1cde0906267aa48b64ea",
+    "url": "/static/js/runtime-main.e525b117.js"
+  },
+  {
+    "revision": "fb7faccb251575e0f33214d091accea1",
+    "url": "/static/media/logo-red.fb7faccb.svg"
+  }
+]);
